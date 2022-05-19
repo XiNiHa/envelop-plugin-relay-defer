4c6419e4a06e6cce409853901cd6c9818b8f5e84
XiNiHa <me@xiniha.dev>
2022-05-19 20:54:52 +0900

    Make options optional

--- index.ts
+++ index.ts
@@ -1,31 +1,34 @@
 import { isAsyncIterable, mapAsyncIterator, type Plugin } from "@envelop/core";
 
 interface Options {
-  noHasNext: boolean;
+  keepHasNext: boolean;
 }
 
-const relayDeferPlugin = ({ noHasNext }: Options): Plugin => ({
-  onExecute() {
-    return {
-      onExecuteDone({ result, setResult }) {
-        if (isAsyncIterable(result)) {
-          setResult(
-            mapAsyncIterator(result, (chunk) => {
-              const { hasNext, ...rest } = chunk;
-              return {
-                ...rest,
-                ...(hasNext && !noHasNext ? { hasNext } : {}),
-                extensions: {
-                  ...rest.extensions,
-                  is_final: !hasNext,
-                },
-              };
-            })
-          );
-        }
-      },
-    };
-  },
-});
+const relayDeferPlugin = (options?: Partial<Options>): Plugin => {
+  const merged = Object.assign({ keepHasNext: true }, options);
+  return {
+    onExecute() {
+      return {
+        onExecuteDone({ result, setResult }) {
+          if (isAsyncIterable(result)) {
+            setResult(
+              mapAsyncIterator(result, (chunk) => {
+                const { hasNext, ...rest } = chunk;
+                return {
+                  ...rest,
+                  ...(hasNext && merged.keepHasNext ? { hasNext } : {}),
+                  extensions: {
+                    ...rest.extensions,
+                    is_final: !hasNext,
+                  },
+                };
+              })
+            );
+          }
+        },
+      };
+    },
+  };
+};
 
 export default relayDeferPlugin;
